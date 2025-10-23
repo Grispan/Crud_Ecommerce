44fa065546436d51b0007268593625d947421aba
PAULO EDUARDO GRISPAN <paulo.egrispan@senacsp.edu.br>
2025-10-22 22:05:56 -0300

    modicações do DAO

--- demo-mvc/src/main/java/com/curso/boot/domain/Cargo.java
+++ demo-mvc/src/main/java/com/curso/boot/domain/Cargo.java
@@ -1,5 +1,17 @@
 package com.curso.boot.domain;
+@Entity
+@Table(name="CARGOS")
+public class Cargo  extends AbstractEntity{
+@Column(nome="nome" nullable=false, unique= true, length=60)
+private String nome;
 
-public class Cargo {
+@ManyToOne
+@JoinColumn(name="id_departamentos_fk")
+private Departamento departamento;
+
+@OneToMany(mappedBy="cargo")
+private List<Funcionario> funcionarios;
+
+
 
 }
--- demo-mvc/src/main/java/com/curso/boot/domain/Endereco.java
+++ demo-mvc/src/main/java/com/curso/boot/domain/Endereco.java
@@ -1,5 +1,29 @@
 package com.curso.boot.domain;
 
-public class Endereco {
+@Entity
+@Table(name = "ENDERECOS")
+public class Endereco extends AbstractEntity<Long> {
 
+    @Column(nullable = false)
+    private String logradouro;
+
+    @Column(nullable = false)
+    private String bairro;
+
+    @Column(nullable = false)
+    private String cidade;
+
+    @Column(nullable = false, length = 2)
+    @Enumerated(EnumType.STRING)
+    private UF uf;
+
+    @Column(nullable = false, length = 9)
+    private String cep;
+
+    @Column(nullable = false, length = 5)
+    private Integer numero;
+
+    private String complemento;
+
+    // métodos get/set omitidos
 }
